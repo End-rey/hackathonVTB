fde504708201ab0fc0818c58c03dc1e027954c89
Андрей Бутусов <andreyis2017@gmail.com>
2022-10-11 12:44:13 +0300

    delete bean

--- JavaPart/Hackathon/src/main/java/com/irvaglan/hackathon/HackathonApplication.java
+++ JavaPart/Hackathon/src/main/java/com/irvaglan/hackathon/HackathonApplication.java
@@ -1,13 +1,8 @@
 package com.irvaglan.hackathon;
 
-import com.irvaglan.hackathon.Entity.Headline;
-import com.irvaglan.hackathon.Entity.NLP;
-import com.irvaglan.hackathon.Repository.HeadlineRepository;
-import com.irvaglan.hackathon.Repository.NLPRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class HackathonApplication {
@@ -16,18 +11,4 @@ public class HackathonApplication {
         SpringApplication.run(HackathonApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner run(NLPRepository nlpRepository, HeadlineRepository headlineRepository) {
-        return (args -> {
-            NLP nlp = new NLP("Not Trendy!", "UnInsightful", "Back-End Developer");
-            Headline headline = new Headline("Cars 3 and KFC", "Cars 3 and KFC are saving people from depression",
-                    "https://vk.com/mrsadpepe");
-            nlpRepository.save(nlp);
-            headlineRepository.save(headline);
-            nlp.setHeadline(headline);
-            headline.setNlp(nlp);
-            nlpRepository.save(nlp);
-            headlineRepository.save(headline);
-        });
-    }
 }
